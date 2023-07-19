fd0867e95775a2e65067605446bc93822ff2efd9
sn0rlax <rodriguezsr429@gmail.com>
2023-07-18 21:57:57 -0700

    Update to limit node count

--- gke.tf
+++ gke.tf
@@ -12,14 +12,25 @@ resource "google_container_cluster" "gke-cluster" {
   location = "us-central1"
   remove_default_node_pool = true
   initial_node_count = 1
+  metadata = {
+    disable-legacy-endpoints = "true"
+  }
 
   network = google_compute_network.vpc_network.id
+  pod_security_policy_config {
+    enabled = true
+  }
   subnetwork = google_compute_subnetwork.network.id
   
+  labels = {
+    developer = "deveng"
+  }
   ip_allocation_policy {
     cluster_secondary_range_name = "tf-subnet-range-2"
     services_secondary_range_name = google_compute_subnetwork.network.secondary_ip_range.0.range_name
   }
+
+  
 }
 
 
@@ -27,11 +38,18 @@ resource "google_container_node_pool" "primary_preemptible_nodes" {
   name       = google_container_cluster.gke-cluster.name
   location   = "us-central1"
   cluster    = google_container_cluster.gke-cluster.name
-  node_count = 3
+  node_count = 2
+  labels = {
+    developer = "deveng"
+  }
+  metadata = {
+    disable-legacy-endpoints = "true"
+  }
+
 
   node_config {
     preemptible  = true
-    machine_type = "e2-standard-4"
+    machine_type = "e2-standard-2"
     oauth_scopes = [
       "https://www.googleapis.com/auth/logging.write",
       "https://www.googleapis.com/auth/monitoring",
