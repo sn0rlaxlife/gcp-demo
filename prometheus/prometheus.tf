c2597ab1d42810747156b38f14f6db3a1ed87e46
sn0rlax <79283899+sn0rlaxlife@users.noreply.github.com>
2023-07-19 19:39:38 -0700

    Update prometheus.tf

--- prometheus/prometheus.tf
+++ prometheus/prometheus.tf
@@ -1,14 +1,3 @@
-provider "kubectl" {
-  config_path = "~/.kube/config"
-
-}
-
-provider "helm" {
-  kubernetes {
-    config_path = "~/.kube/config"
-
-  }
-}
 resource "helm_release" "prometheus" {
   chart            = "kube-prometheus-stack"
   namespace        = "prometheus"
